95edf5c1a76537a77411ddb49171fb3af9510fe8
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-28 17:25:24 +0500

    j

---
+++ jQuery Practical/app.js
@@ -0,0 +1,72 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("body").addClass(".img")
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").addClass("p1")
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").fadeOut(2500)
+//     })
+// })
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("body").css('background-color','red')
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").addClass("p1")
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         let user=prompt("Enter some text")
+//         $("p").prepend(user)
+//     })
+// })
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         let user=prompt("Enter some text")
+//         $("span").html(user)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("input").focus(function(){
+//         alert("Hello")
+       
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("div").hover(function(){
+//         $("div").animate({height:'200px',width:'200px'})
+       
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("div").css('color','yellow')
+       
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         let user=prompt("Enter some text")
+//         $("p").html(user)
+//     })
+// })
+
+
+
